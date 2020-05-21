e86aabf3839283f69aa63d7a9ac28233a8cdd053
Muhammad Faraz Ansar <49414010+muhammadfaraz6640@users.noreply.github.com>
2020-05-21 18:49:14 +0500

    Get Rating By Average

---
+++ GetRating.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="GetRating.aspx.cs" Inherits="StarRating.GetRating" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="margin:36%">
+            <asp:Label ID="Label1" runat="server" ></asp:Label>
+            <img src="Images/FilledStar.jpg" width="20" height="20"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ GetRating.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace StarRating
+{
+    public partial class GetRating : System.Web.UI.Page
+    {
+        public SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Rating"].ToString());
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string query = "select AVG(ALL RValue)AS RVal from Rating";
+            con.Open();
+            SqlCommand com = new SqlCommand(query, con);
+            SqlDataReader rd = com.ExecuteReader();
+            string value="";
+            while(rd.Read())
+            {
+                value = rd["RVal"].ToString();
+            }
+            Label1.Text = value;
+        }
+    }
+}
